d056aed87c395fb0f41eaf7eca37ca1671237e1f
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-05-19 16:46:19 +0700

    mengambil warisan dari baseclass 1 (void perkenalan)

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -14,3 +14,10 @@ public:
         cout << "Hallo saya Function dari base class";
     }
 };
+
+class derivedClass : public baseClass1 {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
