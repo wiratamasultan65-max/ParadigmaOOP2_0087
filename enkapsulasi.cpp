c99e21cbebcd08a895a33ae7e9c658ae76035a9d
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-05-19 15:20:43 +0700

    yang terlihat hanya yang aksesnya public

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -4,3 +4,11 @@ using namespace std;
 class remotelampu {
 private:
     string saklarNo[10];
+public:
+     void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+      string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
