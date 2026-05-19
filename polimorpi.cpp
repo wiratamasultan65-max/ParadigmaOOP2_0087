4b2bd58d5ca7d2b096397e102d75fab375b73313
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-05-19 15:16:22 +0700

    fungsi untuk menampilkan atau mengembalikan nilai

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -21,3 +21,20 @@ public:
         cout << "Pesan dari Lia." << endl;
     }
 };
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+
+    obyek = &b;
+    obyek->pesan();
+
+      // a.seseorang::pesan();
+    // b.seseorang::pesan();
+
+    return 0;
+}
