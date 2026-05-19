2b5e1b141a58817d0de23f97e28d6b17a196d10a
wiratamasultan65-max <wiratamasultan65@gmail.com>
2026-05-19 15:33:36 +0700

    ini fungsi nya untuk mengembalikan fungsi

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+class AbstraksiKlas {
+private: string x, y;
+
+public:
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
+ void display() {
+        cout << "Nilai x: " << x << endl;
+        cout << "Nilai y: " << y << endl;
+    }
+};
+
+int main() {
+    AbstraksiKlas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+    return 0;
+}
